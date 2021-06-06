ff9f600dd63fd644b22d083be4d44d5073925430
Sven Seemann <sven.seemann@bindoc.de>
2021-06-06 16:03:11 +0200

    feat: harvesters are even distributed between sources

--- src/creeps/role/harvester.ts
+++ src/creeps/role/harvester.ts
@@ -1,34 +1,87 @@
 import {CreepImpl} from './creep-impl';
 
+export interface RoomSourceDistribution {
+    [sourceId: string]: number;
+}
+
 export class Harvester extends CreepImpl {
 
     private static _sourcesMemoryKey = 'sources';
 
+    private readonly _sourceId: string;
+
     constructor(creep: Creep) {
         super(creep);
+
+        this._sourceId = this._getCreepMemory().sourceId;
     }
 
     run(): void {
-        this.selectSource();
+        const source = this.selectSource();
 
-        const sources = this._creep.room.find(FIND_SOURCES);
-
-        if(this._creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            this._creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+        if (!!source) {
+            this.harvest(source);
         }
     }
 
-    public getSourcesMemory(room: Room) {
-        return (room.memory as any)[Harvester._sourcesMemoryKey];
+    public selectSource(): Source | null {
+        let source = null;
+
+        if (!!this._sourceId) {
+            source = this._getRoomSources().find(source => source.id === this._sourceId) || null;
+        } else {
+            source = this._selectSourceFromSourceDistribution();
+        }
+
+        return source;
     }
 
-
-
-    public selectSource() {
-        const sources = this._creep.room.find(FIND_SOURCES);
+    public harvest(source: Source) {
+        if (this._creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            this._creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+        }
     }
 
-    public harvest() {
+    private _selectSourceFromSourceDistribution(): Source | null {
+        const sources = this._getRoomSources();
+        const sourceDistribution = this._getRoomSourceDistribution();
 
+        const sourceIds = Object.keys(sourceDistribution);
+        let resultSource: Source | null;
+
+        if (sourceIds.length === 0) {
+            sources.forEach(source => sourceDistribution[source.id] = 0);
+            resultSource = sources[0];
+        } else {
+            let lowestSourceId = sourceIds[0];
+            sourceIds.forEach(sourceId => {
+                if (sourceDistribution[lowestSourceId] > sourceDistribution[sourceId]) {
+                    lowestSourceId = sourceId;
+                }
+            });
+
+            resultSource = sources.find(source => source.id === lowestSourceId) || null;
+        }
+
+        if (!!resultSource) {
+            this._getCreepMemory().sourceid = resultSource.id;
+            sourceDistribution[resultSource.id] += 1;
+            this._setRoomSourceDistribution(sourceDistribution);
+        }
+
+        return resultSource;
+    }
+
+    private _getRoomSourceDistribution(): RoomSourceDistribution {
+        return (this._creep.room.memory as any)[Harvester._sourcesMemoryKey] as RoomSourceDistribution ||
+            {};
+    }
+
+    private _setRoomSourceDistribution(sourceDistribution: RoomSourceDistribution) {
+        (this._creep.room.memory as any)[Harvester._sourcesMemoryKey] = sourceDistribution;
+    }
+
+    private _getRoomSources() {
+        return this._creep.room.find(FIND_SOURCES);
     }
 }
