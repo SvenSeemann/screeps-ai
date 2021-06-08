adce0057c316a7f8f07facfab8b0d1189b39adff
Sven Seemann <sven.seemann@bindoc.de>
2021-06-08 08:52:10 +0200

    fix: fixed query room for towers

--- src/structures/tower-handler.ts
+++ src/structures/tower-handler.ts
@@ -3,7 +3,11 @@ export class TowerHandler {
     constructor(
         private _room: Room
     ) {
-        const towers: StructureTower[] = this._room.find(STRUCTURE_TOWER as any) as any;
+        const towers: StructureTower[] = this._room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return structure.structureType == STRUCTURE_TOWER;
+            }
+        }) as StructureTower[];
 
         for (let tower of towers) {
             const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
